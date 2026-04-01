e5d7d38334d4d01978fae230905a1390ee288add
aida030313-coder <aida030313@g.eulji.ac.kr>
2026-04-02 01:49:09 +0900

    0331 mission upload

--- mission/0331/0331_mission/src/components/Mission.jsx
+++ mission/0331/0331_mission/src/components/Mission.jsx
@@ -2,8 +2,28 @@ import { useEffect, useState } from 'react'
 
 function Mission() {
 
+    const [prom, setProm] = useState('');
+    const [inputText, setInputText] = useState('');
+    const [startTime, setStartTime] = useState(0);
+
+    const handleEnter = (e) => {
+        if (e.key === 'Enter') {
+            if (prom === inputText) {
+
+                const alertTime = Date.now() - startTime;
+                alert(`${alertTime}(ms)`)
+                
+                setProm('');
+                setInputText('');
+                setStartTime(Date.now());
+            }
+        }
+    }
+
     useEffect(() => {
         const prom = prompt("문자열을 입력해주세요. " );
+        setProm(prom);
+        setStartTime(Date.now())
     }, []);
 
 
@@ -12,6 +32,11 @@ function Mission() {
             <h2>{prom}</h2>
             <input
                 type="text"
+                value={inputText}
+                onKeyDown={handleEnter}
+
+                // input에 타이핑이 쳐지도록.
+                onChange={(e) => setInputText(e.target.value)}
             />
         </>
     );
