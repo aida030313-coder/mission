ac9fa2ef06862820359e7e5b2e2978682d0eaff3
aida030313-coder <aida030313@g.eulji.ac.kr>
2026-04-06 23:39:49 +0900

    mission ongoing

--- mission/0402/0402_mission/src/components/num1_ToDo.jsx
+++ mission/0402/0402_mission/src/components/num1_ToDo.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function num1_ToDo() {
+
+    const [inputValue, setInputValue] = useState('');
+    const [Todos, setTodos] = useState([]);
+
+    const handleAddTodo = () => {
+        setTodos([
+            ...Todos,
+            inputValue
+        ])
+
+        setInputValue('');
+    }
     return (
-        <div>
+        <>
             <h2>할일 목록</h2>
             <input
+                type="text"
                 placeholder='새 할일'
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
             />
-            <button>추가</button>
-        </div>
+            <button onClick={handleAddTodo}>추가</button>
+
+            {Todos.map((Todo, index) => <div key={index}>{Todo}</div>)}
+        </>
     )
 }
 
--- mission/0402/0402_mission/src/components/num4_Login.jsx
+++ mission/0402/0402_mission/src/components/num4_Login.jsx
@@ -4,10 +4,17 @@ function num4_Login() {
     return (
         <div>
             <h2>로그인 폼</h2>
-            사용자명: <input/>
+            사용자명: 
+            <input
+                placeholder='사용자명'
+            />
             <br/>
-            비밀번호: <input/>
+            비밀번호: 
+            <input
+                placeholder='비밀번호'
+            />
             <br/>
+            
             <input type='checkbox'/> 로그인 상태 유지 <br/>
             <button>로그인</button>
         </div>
